dd54c57e8ecd75bd6a6502b3a29bd1b8cb9f857c
David Cardenas <davidcardenas1993@yahoo.com>
2019-11-07 19:40:57 -0700

    added base logic for routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,11 @@
+var path = require("path");
+
+module.exports = function() {
+    app.get("/api/friends", function(req, res) {
+
+    });
+
+    app.post("/api/friends", function(req, res) {
+
+    });
+}
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,11 @@
+var path = require("path");
+
+module.exports = function() {
+    app.get("/survey", function(req, res) {
+        
+    })
+
+    app.get("/", function(req, res) {
+
+    })
+}
