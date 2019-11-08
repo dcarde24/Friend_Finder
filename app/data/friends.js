5da25465a3cd44e514f4571f71eb2b2f3ed8cbe1
David Cardenas <davidcardenas1993@yahoo.com>
2019-11-08 10:47:55 -0700

    added more people into the friends list to build a larger array of people

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 module.exports = [
     {
         "name":"Bill",
-        "photo":"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwia35r8zNnlAhXKpJ4KHRzvBKQQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.cbsnews.com%2Fpictures%2Ffavorite-cartoons%2F&psig=AOvVaw2QLNjbVcyivfmui2F355AO&ust=1573267673027206",
+        "photo":"https://i.pinimg.com/474x/4a/d0/1d/4ad01dec05b1f592066e9d78b9fc6762--ice-bear-cartoon-polar-bears.jpg",
         "scores":[
             5,
             5,
@@ -18,7 +18,7 @@ module.exports = [
     
       {
         "name":"Rex",
-        "photo":"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjA-ZmPzdnlAhUGv54KHce8CosQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.hulu.com%2Fgenre%2Fcartoons-915cbde7-6a2a-42b3-9951-5a635393c462&psig=AOvVaw2QLNjbVcyivfmui2F355AO&ust=1573267673027206",
+        "photo":"http://i.imgur.com/nBYOnvl.jpg",
         "scores":[
             1,
             1,
@@ -48,6 +48,124 @@ module.exports = [
             4,
             1
           ]
-      }
+      },
       
+      {
+        "name":"Bob",
+        "photo":"https://atgbcentral.com/data/out/180/5572937-cartoon.jpg",
+        "scores":[
+            2,
+            1,
+            5,
+            5,
+            5,
+            4,
+            3,
+            3,
+            4,
+            5
+          ]
+      },
+
+      {
+        "name":"Sara",
+        "photo":"https://i.pinimg.com/236x/30/86/39/30863997ec10f73b4c66540f284ceb61--old-cartoons-cartoon-girls.jpg",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+          ]
+      },
+
+      {
+        "name":"Wanda",
+        "photo":"https://avatar.amuniversal.com/feature_avatars/recommendation_images/features/ca/large_rec-201701251555.jpg",
+        "scores":[
+            5,
+            5,
+            1,
+            1,
+            2,
+            1,
+            3,
+            5,
+            3,
+            2
+          ]
+      },
+
+      {
+        "name":"Randall",
+        "photo":"http://monsul.co/wp-content/uploads/2019/05/monsters-inc-randall-land-pin-monsters-inc-randall-monsters-inc-assistant-lips.jpg",
+        "scores":[
+            1,
+            1,
+            1,
+            3,
+            4,
+            5,
+            5,
+            5,
+            5,
+            2
+          ]
+      },
+
+      {
+        "name":"Larry",
+        "photo":"http://www.cartoonbucket.com/wp-content/uploads/2016/01/Larry-The-Lobster-Picture-fg45615.jpg",
+        "scores":[
+            3,
+            3,
+            5,
+            2,
+            1,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"Jorge",
+        "photo":"https://i.pinimg.com/originals/02/e0/34/02e03435444163a80518a482f5d655bc.jpg",
+        "scores":[
+            3,
+            1,
+            4,
+            4,
+            5,
+            5,
+            3,
+            2,
+            1,
+            5
+          ]
+      },
+
+      {
+        "name":"Sergio",
+        "photo":"https://imagej.net/images/baboon.jpg",
+        "scores":[
+            3,
+            4,
+            3,
+            3,
+            1,
+            5,
+            3,
+            2,
+            2,
+            3
+          ]
+      }
 ]
